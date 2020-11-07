a4bfab1b5659352f4e924886bbe6a3ee47bfac57
Ankit <ankitgond001@gmail.com>
2020-11-07 20:33:46 +0530

    Jenkins github conf

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld{
+	public static void main(String[] args) {
+		//Print "Hello, World " in the therminal windows
+		System.out.println("Hello World for GitHub Jenkins");
+	}
+}
